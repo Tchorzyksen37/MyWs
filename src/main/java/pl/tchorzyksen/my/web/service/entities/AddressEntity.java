f7ab162a7316847fbc234ff1051b26f7d4d91d8d
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2022-10-13 23:28:39 +0200

    Create AddressEntity - add user to address relation - create AbstractEntity - keep track of create and update timestamp in Entities - create flyway migration (MANUALLY because flyway does not support diff ://)

---
+++ src/main/java/pl/tchorzyksen/my/web/service/entities/AddressEntity.java
@@ -0,0 +1,28 @@
+package pl.tchorzyksen.my.web.service.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import pl.tchorzyksen.my.web.service.model.Country;
+
+@Entity(name = "addresses")
+public class AddressEntity extends AbstractEntity {
+
+  @Column(name = "street")
+  private String street;
+
+  @Column(name = "postal_code")
+  private String postalCode;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "country")
+  private Country countryCode;
+
+}
