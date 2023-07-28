e6b14b1ecb374e17bab13faa347946636ba73215
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2023-07-28 12:30:36 +0200

    Create integrationTest module

--- src/integrationTest/groovy/pl/tchorzyksen/my/service/backend/controller/GreetingControllerIntegrationSpec.groovy
+++ src/integrationTest/groovy/pl/tchorzyksen/my/service/backend/controller/GreetingControllerIntegrationSpec.groovy
@@ -1,18 +1,17 @@
-package pl.tchorzyksen.my.web.service.controller
+package pl.tchorzyksen.my.service.backend.controller
 
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import pl.tchorzyksen.my.web.service.AbstractFunctionalSpec
-import pl.tchorzyksen.my.web.service.orm.GreetingEntity
+import pl.tchorzyksen.my.service.backend.AbstractIntegrationSpec
+import pl.tchorzyksen.my.service.backend.orm.GreetingEntity
 
-class GreetingControllerFunctionalSpec extends AbstractFunctionalSpec {
+class GreetingControllerIntegrationSpec extends AbstractIntegrationSpec {
 
   void "get #requestParam greeting #result "(String requestParam, String result) {
     given: "uri"
     String uri = requestParam == null ? "/greeting" : "/greeting?name=${requestParam}"
 
     when:
-    ResponseEntity<GreetingEntity> response = get(uri, GreetingEntity.class)
+    def response = get(uri, GreetingEntity.class)
 
     then:
     response.statusCode == HttpStatus.OK
--- src/integrationTest/groovy/pl/tchorzyksen/my/service/backend/controller/UserControllerIntegrationSpec.groovy
+++ src/integrationTest/groovy/pl/tchorzyksen/my/service/backend/controller/UserControllerIntegrationSpec.groovy
@@ -1,7 +1,5 @@
-package pl.tchorzyksen.my.web.service.controller
+package pl.tchorzyksen.my.service.backend.controller
 
-import java.time.LocalDateTime
-import javax.sql.DataSource
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,19 +8,20 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import pl.tchorzyksen.my.web.service.AbstractFunctionalSpec
-import pl.tchorzyksen.my.web.service.exception.ResourceNotFoundException
-import pl.tchorzyksen.my.web.service.exception.model.ExceptionResponse
-import pl.tchorzyksen.my.web.service.model.request.PersonRequest
-import pl.tchorzyksen.my.web.service.model.request.UserRequest
-import pl.tchorzyksen.my.web.service.model.response.UserPageableResponse
-import pl.tchorzyksen.my.web.service.model.response.UserResponse
-import pl.tchorzyksen.my.web.service.repositories.UserRepository
-import pl.tchorzyksen.my.web.service.service.impl.UserServiceImpl
+import pl.tchorzyksen.my.service.backend.AbstractIntegrationSpec
+import pl.tchorzyksen.my.service.backend.exception.model.ExceptionResponse
+import pl.tchorzyksen.my.service.backend.model.request.PersonRequest
+import pl.tchorzyksen.my.service.backend.model.request.UserRequest
+import pl.tchorzyksen.my.service.backend.model.response.UserPageableResponse
+import pl.tchorzyksen.my.service.backend.model.response.UserResponse
+import pl.tchorzyksen.my.service.backend.repositories.UserRepository
 import spock.lang.Shared
 
+import javax.sql.DataSource
+import java.time.LocalDateTime
+
 @Import(FlywayConfigUsers.class)
-class UserControllerFunctionalSpec extends AbstractFunctionalSpec {
+class UserControllerIntegrationSpec extends AbstractIntegrationSpec {
 
   @Shared
   private static final Set<UserResponse> users = new HashSet<>()
@@ -41,10 +40,10 @@ class UserControllerFunctionalSpec extends AbstractFunctionalSpec {
   def setupSpec() {
     // TODO: Set up const data time for tests
     users.add(new UserResponse(id: 0, version: 0, createdDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"),
-        lastModifiedDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"), userId: "userId0", email: "email0@domain.com"))
+            lastModifiedDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"), userId: "userId0", email: "email0@domain.com"))
 
     users.add(new UserResponse(id: 1, version: 0, createdDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"),
-        lastModifiedDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"), userId: "userId1", email: "email1@domain.com"))
+            lastModifiedDateTime: LocalDateTime.parse("2022-10-14T17:59:57.165952"), userId: "userId1", email: "email1@domain.com"))
   }
 
   void "Should return pageable user response"() {
@@ -76,9 +75,10 @@ class UserControllerFunctionalSpec extends AbstractFunctionalSpec {
     given:
     String alreadyExistingEmailAddress = "email0@domain.com"
     when:
-    ResponseEntity<ExceptionResponse> response = post("/user",
-        new UserRequest(email: alreadyExistingEmailAddress, password: "test123",
-            person: new PersonRequest(firstName: "John", lastName: "Doe")), ExceptionResponse.class)
+    def response
+    response = post "/user",
+            new UserRequest(email: alreadyExistingEmailAddress, password: "test123",
+                    person: new PersonRequest(firstName: "John", lastName: "Doe")), ExceptionResponse.class
 
     then:
     response.statusCode == HttpStatus.BAD_REQUEST
@@ -88,8 +88,8 @@ class UserControllerFunctionalSpec extends AbstractFunctionalSpec {
   void "Create user happy path"() {
     when:
     ResponseEntity<UserResponse> response = post("/user",
-        new UserRequest(person: new PersonRequest(firstName: "John", lastName: "Doe"),
-            email: "test@test.com", password: "test123"), UserResponse.class)
+            new UserRequest(person: new PersonRequest(firstName: "John", lastName: "Doe"),
+                    email: "test@test.com", password: "test123"), UserResponse.class)
 
     then:
     response.statusCode == HttpStatus.CREATED
@@ -104,8 +104,8 @@ class UserControllerFunctionalSpec extends AbstractFunctionalSpec {
     @Bean
     Flyway flyway() {
       var configuration = new FluentConfiguration()
-          .dataSource(dataSource)
-          .locations("db/migration", "test-data/users")
+              .dataSource(dataSource)
+              .locations("db/migration", "test-data/users")
 
       return new Flyway(configuration)
     }
