31b8e33345a1acf3124901fc63b68eed34c90008
Michal Tchorzewski <Michal.Tchorzewski@circlekeurope.com>
2024-01-10 23:02:37 +0100

    Add PostRepository#findById method
    - Use optimistic force increment

--- src/main/java/pl/tchorzyksen/my/service/backend/repositories/PostRepository.java
+++ src/main/java/pl/tchorzyksen/my/service/backend/repositories/PostRepository.java
@@ -1,10 +1,18 @@
 package pl.tchorzyksen.my.service.backend.repositories;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import pl.tchorzyksen.my.service.backend.orm.post.Post;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import static jakarta.persistence.LockModeType.OPTIMISTIC_FORCE_INCREMENT;
+
 public interface PostRepository extends CrudRepository<Post, UUID> {
+
+  @Override
+  @Lock(value = OPTIMISTIC_FORCE_INCREMENT)
+  Optional<Post> findById(UUID id);
 
 }
